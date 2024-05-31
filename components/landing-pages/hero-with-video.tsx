ccbc24e8bb8520eb327365257d82817cff2edec2
Eric Zhang <ezhang91@yahoo.com>
2024-05-31 15:43:04 -0400

    updated landing page

--- components/landing-pages/hero-with-video.tsx
+++ components/landing-pages/hero-with-video.tsx
@@ -11,7 +11,7 @@ import { BorderBeam } from "@/components/ui/border-beam";
 
 export function HeroWithVideo() {
   return (
-    <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 pt-20 text-center lg:pt-32 relative">
+    <div className="mx-auto max-w-10xl px-4 sm:px-6 lg:px-8 pt-20 text-center lg:pt-32 relative">
       <div className="mx-auto max-w-5xl">
         <h1 className="text-balance bg-gradient-to-br from-black from-30% to-black/60 bg-clip-text py-6 text-5xl font-semibold leading-none tracking-tighter text-transparent dark:from-white dark:to-white/40 sm:text-6xl md:text-7xl lg:text-7xl">
           ACE your interview using industry standard AI Copilot.
@@ -41,25 +41,17 @@ export function HeroWithVideo() {
             <div className="relative rounded-xl">
               <BorderBeam />
 
-              <Image
-                src={"/demo-v1.png"}
-                alt="demo-v1"
-                height="1442"
-                width="2432"
-                decoding="async"
-                priority
-                className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-gray-900/10 transition-opacity duration-100 hover:opacity-50 cursor-pointer"
-              />
-
-              <div className="absolute inset-0 bg-white-500 hover:bg-slate-50 opacity-50 flex items-center justify-center rounded-xl transition-opacity duration-300">
-                <div className="bg-black rounded-full p-4 -mt-24 opacity-100">
-                  <PlayCircle className="text-white w-12 h-12 md:h-16 md:w-16 lg:h-20 lg:w-20 xl:h-20 xl:w-20" />
-                </div>
-              </div>
+              <video
+                src="copilot-demo1.mp4"
+                className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+                autoPlay
+                loop
+                muted
+              ></video>
             </div>
-            <div className="relative">
-              <div className="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-white pt-[7%]" />
-            </div>
+            {/* <div className="relative">
+              <div className="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-white pt-[10%]" />
+            </div> */}
           </div>
         </div>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,6 +38,17 @@ module.exports = withUt({
             "offset-distance": "100%",
           },
         },
+        "shine-pulse": {
+          "0%": {
+            "background-position": "0% 0%",
+          },
+          "50%": {
+            "background-position": "100% 100%",
+          },
+          to: {
+            "background-position": "0% 0%",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
